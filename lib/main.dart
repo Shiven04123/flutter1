d97d31bf4fa7031f5797d054c8f20952ca5e04bc
Shiven Bansal <shivenapj@gmail.com>
2025-02-23 14:15:35 +0530

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(CanteenApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Firebase Setup Done")),
+        body: Center(child: Text("Firebase is successfully configured!")),
+      ),
+    );
+  }
+}
+
 
 class CanteenApp extends StatelessWidget {
   @override
